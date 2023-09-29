ea200722f4aa174448860c1ea982589d24aada63
Mathin21 <114302149+Mathin21@users.noreply.github.com>
2023-09-29 16:02:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openAddBookPage() {
+    window.location.href = "add_book.html";
+}
+function openDeleteBookPage(){
+    window.location.href = "delete_book.html";
+}
+function openViewBookPage(){
+    window.location.href = "view_book.html";
+}
+function openIssueBookPage(){
+    window.location.href = "issue_book.html";
+}
+function openReturnBookPage(){
+    window.location.href = "return_book.html";
+}
